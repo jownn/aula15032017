9b8966e77aea1083d657e3f84a3f9f2c817c2a48
152376 <152376@IA09.labinfo.facens.br>
2017-03-15 22:28:09 -0300

    afasfasf

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,32 @@
+package sistema.modelos;
+
+
+
+public class Professor {
+
+
+	private int codigo;
+	private String nome;
+	private String curso;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+
+}
